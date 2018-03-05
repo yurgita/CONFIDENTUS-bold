44fa654b7b0fb68d42ea865b70d86969746d9d0e
yurgita <jurgita.kukarskiene@gmail.com>
2018-03-05 11:54:21 +0200

    scroll, tinklas

--- CONFIDENTUS-website/HTML/assets/js/scripts.js
+++ CONFIDENTUS-website/HTML/assets/js/scripts.js
@@ -2,7 +2,7 @@
 /* Portfolio */
 $(window).load(function() {
     var $cont = $('.portfolio-group');
-    
+
     $cont.isotope({
         itemSelector: '.portfolio-group .portfolio-item',
         masonry: {columnWidth: $('.isotope-item:first').width(), gutterWidth: -20, isFitWidth: true},
@@ -67,10 +67,10 @@ $(document).ready(function(){
 
 // thumbs animations
 $(function () {
-    
+
     $(".thumbs-gallery i").animate({
              opacity: 0
-    
+
           }, {
              duration: 300,
              queue: false
@@ -86,12 +86,12 @@ $(function () {
              queue: false
           });
    });
- 
+
    $(".thumbs-gallery i").hover(
       function () {
           $(this).animate({
              opacity: 0
-    
+
           }, {
              duration: 300,
              queue: false
