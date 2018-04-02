c8fb6d14288e02bc8b2be7c06ade45478300b175
yurgita <jurgita.kukarskiene@gmail.com>
2018-04-02 14:58:06 +0300

    pastabeles

--- CONFIDENTUS-website/HTML/assets/js/scripts.js
+++ CONFIDENTUS-website/HTML/assets/js/scripts.js
@@ -148,3 +148,10 @@ $(function(){
     });
   }
 });
+
+
+// Pop up pictures
+$("#pop").on("click", function() {
+   $('#imagepreview').attr('src', $('#myImg').attr('src')); // here asign the image to the modal when the user click the enlarge link
+   $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
